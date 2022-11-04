db85df3f19fc6d399fdf1163e1d2ca8ffe7a71eb
muham <muhammadaaa727@gmail.com>
2022-11-04 22:22:31 +0300

    import package

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:testss/register.dart';
+import 'package:temp_project/register.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
